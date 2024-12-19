040820bb99e23edc992cace2eb03b02324260579
Ayoub Ebn El Hadj <ayoubbelhadj1998@gmail.com>
2024-12-19 18:34:36 +0100

    hamdi commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('A7la ayoub fik ye tekup!!!!!!!!!!!');
+    res.send('A7la hamdi fik ye tekup!!!!!!!!!!!');
 });
 
 app.listen(PORT, () => {
