8badf48ceccb43827f14385e47b1e8a6dd73c18c
Ayoub Ebn El Hadj <ayoubbelhadj1998@gmail.com>
2024-12-23 10:27:56 +0100

    hamdii

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello  hh Marwa !!!!!!!!');
+    res.send('Hello Marwa !!!!!!!!');
 });
 
 app.listen(PORT, () => {
