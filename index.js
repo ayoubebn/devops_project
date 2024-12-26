321781d288f932d2448d3c1a3e82d5e66137ce81
Ayoub Ebn El Hadj <ayoubbelhadj1998@gmail.com>
2024-12-26 10:00:07 +0100

    hello ayoub ebn el hadj

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-
 const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// Middleware pour analyser le JSON dans les requêtes
+app.use(express.json());
+
+// Route principale
 app.get('/', (req, res) => {
-    res.send('Hello NOUR HENTATI -1 ');
+    res.send('Hello hello Ayoub Ebn EL Hadj ');
 });
 
+// Route pour gérer le webhook GitHub
+app.post('/github-webhook', (req, res) => {
+    console.log('Webhook reçu:', req.body);
+    res.status(200).send('Webhook traité avec succès');
+});
+
+// Démarrer le serveur
 app.listen(PORT, () => {
     console.log(`App running on http://localhost:${PORT}`);
 });
