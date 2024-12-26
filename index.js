03890ed3e05ef8ea0e82c92c26b5edaa0627f4eb
Ayoub Ebn El Hadj <ayoubbelhadj1998@gmail.com>
2024-12-26 09:46:03 +0100

    hello ayoub

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello Ayoub EBN EL HADJ ');
+    res.send('Hello Ayoub EBN EL HADJ hello ');
 });
 
 app.listen(PORT, () => {
